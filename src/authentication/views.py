e34aaee15ddb3811fa1353ff3554b2a1a63d795e
0nah1d <nahidthedeveloper@gmail.com>
2025-08-20 16:56:20 +0600

    add filter backend

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 
 from authentication.serializers.user_serilizer import UserSerializer
@@ -14,3 +15,5 @@ class UsersViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
     http_method_names = ['get']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['username', 'email', 'first_name', 'last_name']
